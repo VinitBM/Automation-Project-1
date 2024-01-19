278d5d0af54e71737a9c84b8d1b1cf51ad916df9
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 12:00:53 +0530

    Update AutomatedTest.java

--- Automation exclr/src/AutomatedTest.java
+++ Automation exclr/src/AutomatedTest.java
@@ -1,45 +1,42 @@
-package SeleniumProject;
+package automationexercise;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.open.selenium.WebElement;
-import org.openqa.selenium.chrom.ChromDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class AutomatedTest extends Abstract {
+    public AutomatedTest(WebDriver driverhere) {
+		super(driverhere);
+	
+	}
 
-public class AutomatedTest extends AbstractMethods {
-	WebDriver driver;{
+	public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\MOVIES\\chromedriver-win64.zip\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationexercise.com");
 
-public static void main(String[] args){
+        WebElement categoriesSidebar = driver.findElement(By.xpath("//h2[normalize-space()='Category']"));
+        if (categoriesSidebar.isDisplayed()) {
+            System.out.println("Categories are visible on the left sidebar");
+        }
 
-System.setProperty(“"webdriver.chrome.driver", "E:/loopsautomation/src/main/resources/chromedriver.exe");
-WebDriver driver = new ChromDriver();
-Driver.get("http:automationexercise.com");
-WebElement categoriesSidebar = Driver.findElement(“h2[normalize-space()="Category"]”);
-If (categoriesSidebar.isDisplay())
-{
-System.out.println(“Categories are visible on the left sidebar”);
+        WebElement womenCategory = driver.findElement(By.xpath("//a[normalize-space()='Women']"));
+        womenCategory.click();
+
+        WebElement dressCategory = driver.findElement(By.xpath("//h2[@class='title text-center']"));
+        dressCategory.click();
+
+        WebElement categoryPageText = driver.findElement(By.xpath("//h2[@class='title text-center']"));
+        if (categoryPageText.getText().contains("Women-Top Product")) {
+            System.out.println("Category page for Women-Top Product is displayed.");
+        } else {
+            System.out.println("Category page for Women-Top Products is not displayed");
+        }
+
+        WebElement menSubCategory = driver.findElement(By.xpath("//a[normalize-space()='Men']"));
+        menSubCategory.click();
+
+        driver.quit();
+    }
 }
-
-WebElement womenCategory = driver.findElement(a[normalize-space()='Women);
-WomenCategory.click();
-WebElement dressCategory = driver.findElement(By.xpath(“h2[@class='title text-center]”));
-dressCategory.click();
-
-WebElement categoryPageText = driver.findElement(By.xpath(“h2[@class="title text-center"]”));
-If(categoryPageText.getText().contains(“Women-Top Product”))
-{
-System.out.println(“Category page for Women- Top Product  is displayed.”);
-}
-Else
-{
-System.out.println(“Category page for Women- Top Products is not displayed”);
-}
-
-WebElement menSubCategory = driver.findElement(By.xpath(“a[normalize-space()="Men"]”));
-menSubCategory.click();
-
-driver.quit();
-}
-}
-
-
