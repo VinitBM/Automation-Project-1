e62303f7e96e8f47b5333cef0f33cd8e4d0fa7ed
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 13:17:34 +0530

    Update Product.java

--- Automation exclr/src/Product.java
+++ Automation exclr/src/Product.java
@@ -1,53 +1,48 @@
-
-package SeleniumProject;
+package automationexercise;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
 
-public class Product extends AbstractMethods {
-        WebDriver driver;{
+public class Product extends Abstract{
+    WebDriver driver;
 
-public static void main(String[] args){
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\MOVIES\\chromedriver-win64.zip\\New folder\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
-System.setProperty(“"webdriver.chrome.driver", "E:/loopsautomation/src/main/resources/chromedriver.exe");
-WebDriver driver = new ChromeDriver();
+        driver.get("http://automationexercise.com");
 
-driver.get(http:automationexercise.com);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-          driver.manage()timeouts().implicitlyWait (Duration.  oFSeconds(10));
-try{
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-Thread.sleep(6000);
-      
-        
-}
-Catch(InterruptedException e){
-e.printStackTrace();
-WebElement productsButton = driver.findElement(By.linkText(“Products”));
+        WebElement productsButton = driver.findElement(By.linkText("Products"));
+        productsButton.click();
 
-productsButton.click();
         WebElement viewProductButton = driver.findElement(By.xpath("(//a[@class='btn btn-primary'])[1]"));
         viewProductButton.click();
- WebElement productName = driver.findElement(By,id(“product_name”));
-WebElement category = driver.findElement(by.id(“product_cayegory”));
-WebElement price = driver.findElement(By.id(“Product_price”));
-WebElement availability = driver.findElement(By.id(“product_availabilty”));
-WebElement condtion = driver.findElement(By.id(“product_condtion”));
-WebElement condtion = driver.findElement(By.id(“product_brand”));
 
+        WebElement productName = driver.findElement(By.id("product_name"));
+        WebElement category = driver.findElement(By.id("product_category"));
+        WebElement price = driver.findElement(By.id("Product_price"));
+        WebElement availability = driver.findElement(By.id("product_availability"));
+        WebElement condition = driver.findElement(By.id("product_condition"));
+        WebElement brand = driver.findElement(By.id("product_brand"));
 
+        if (productName.isDisplayed() && category.isDisplayed() && price.isDisplayed() &&
+                availability.isDisplayed() && condition.isDisplayed() && brand.isDisplayed()) {
+            System.out.println("Product details are visible");
+        } else {
+            System.out.println("Product details are not visible");
+        }
 
-If(productName.isDisplayed()&& category.isDisplayed()&&price.isDisplayed()&&availability.isDisplayed()&&condition.isDisplayed()&&brand.isDisplayed())
-{
-System.out.println(“Product details are visible”);
+        driver.quit();
+    }
 }
-Else{
-
-System.out.println(“Product details are not visible”);
-}
-Driver.quit();
-}
-}
-
