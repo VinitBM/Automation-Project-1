db1d64e7e7d54a932df4cdee715af3c13610a34f
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 13:52:20 +0530

    Update ProductSearchTest.java

--- Automation exclr/src/ProductSearchTest.java
+++ Automation exclr/src/ProductSearchTest.java
@@ -1,49 +1,57 @@
+package automationexercise;
 
-package SeleniumProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.list;
+import java.time.Duration;
+import java.util.List;
 
-public class ProductSearchTestextends AbstractMethods {
-    WebDriver driver;{
+public class ProductSearchTest extends Abstract{
+    WebDriver driver;
 
-public static void main(String[]args){
-System.setProperty(“"webdriver.chrome.driver", "E:/loopsautomation/src/main/resources/chromedriver.exe");
-WebDriver driver = new ChromeDriver();
-Driver.get(http:automationexercise.com);
-      driver.manage()timeouts().implicitlyWait (Duration.  oFSeconds(10));
-Try{
-Thread.sleep(5000);
-    driver.manage().timeouts().implicitlyWait (Duration.  oFSeconds(10));
-    
-}
-Catch(InterruptedException e){
-e.printStackTrace();
-}
-WebElement productsButton = driver.findElement(By.linkText("Products"));
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\MOVIES\\chromedriver-win64.zip\\New folder\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationexercise.com");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement productsButton = driver.findElement(By.linkText("Products"));
         productsButton.click();
-String productNameToSearh = “ Sleeves Printed Tshirts”;
-WebElement searchInput = driver.findElement(By.id(“Search_input”));
-searchInput.sendKeys(productNameToSearch);
 
-WebElement searchButton = driver.findElement(By.id(“search_button”));
-searchButton.click();
-WebElement searchedProductsTitle = driver.findElement(By.xpath("//h2[contains(text(), 'SEARCHED PRODUCTS')]"));
-If(SearchedProductsTitle.isDisplayed(){
-System.out.println(“Searched Products’visible section”);
-}
-Else{
-System.out.println(“Searche Products section is not visible”);
-}
+        String productNameToSearch = "Sleeves Printed Tshirts";
+        WebElement searchInput = driver.findElement(By.id("Search_input"));
+        searchInput.sendKeys(productNameToSearch);
+
+        WebElement searchButton = driver.findElement(By.id("search_button"));
+        searchButton.click();
+
+        WebElement searchedProductsTitle = driver.findElement(By.xpath("//h2[contains(text(), 'SEARCHED PRODUCTS')]"));
+
+        if (searchedProductsTitle.isDisplayed()) {
+            System.out.println("Searched Products section is visible");
+        } else {
+            System.out.println("Searched Products section is not visible");
+        }
+
         List<WebElement> searchResults = driver.findElements(By.className("product-item"));
+
         if (searchResults.size() > 0) {
             System.out.println("Related products are visible after search:");
             for (WebElement result : searchResults) {
-                System.out.println(result.getText()); 
+                System.out.println(result.getText());
             }
         } else {
             System.out.println("No related products found after search.");
         }
-        driver.
+
+        driver.quit();
+    }
+}
