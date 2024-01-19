aa9ba7381863af6d0e7d23a9100f642b76c8d8a7
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 11:27:13 +0530

    Update Abstract.java

--- Automation exclr/New folder1/Abstract.java
+++ Automation exclr/New folder1/Abstract.java
@@ -1,5 +1,7 @@
-package utilities;
+package automationexercise;
+
 import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,24 +9,25 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Abstract{
+public class Abstract {
 
-	WebDriver driver;
-	
-	public Abstract(WebDriver driverhere) {
-		this.driver = driverhere;	
-	}
-	public void Wait Till Link Is Clickable(By FindBy) {
-		
-		WebDriverWait wait=new WebDriverWait(driver,Duration ofSeconds(20));
-		wait.until(ExpectedConditions.elementToBeClickable(FindBy));				
-	}
-	public void Hoveranyelement(WebElement el) {
-		
-		Actions ac = new Actions(driver);
-		ac.moveToElement(el).build().perform();
-	}
-	public void implicitlywaitmethod(){
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-	}
+    WebDriver driver;
+
+    public Abstract(WebDriver driverhere) {
+        this.driver = driverhere;
+    }
+
+    public void waitTillLinkIsClickable(By findBy) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable(findBy));
+    }
+
+    public void hoverAnyElement(WebElement el) {
+        Actions ac = new Actions(driver);
+        ac.moveToElement(el).build().perform();
+    }
+
+    public void implicitlyWaitMethod() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+    }
 }
