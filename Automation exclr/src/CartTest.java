dd928b66a7c7716783941f14f27ada9b215ffe93
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 12:54:24 +0530

    Update CartTest.java

--- Automation exclr/src/CartTest.java
+++ Automation exclr/src/CartTest.java
@@ -1,46 +1,49 @@
+package automationexercise;
 
-package SeleniumProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
 
-Public class CartTest extends AbstractMethods {
-    WebDriver driver;{
+public class CartTest extends Abstract {
+    WebDriver driver;
 
-Public static void main(String[]args){
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\MOVIES\\chromedriver-win64.zip\\New folder\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get("http://automationexercise.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-System.setProperty(“"webdriver.chrome.driver", "E:/loopsautomation/src/main/resources/chromedriver.exe");
-WebDriver driver = new ChromeDriver();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-Driver.get(http:automationexercise.com);
-    driver.manage().timeouts().implicitlyWait (Duration.   oFSeconds(10));
-Try{
-Thread.sleep(5000);
-    
-}
-Catch(InterruptExeption e){
-e.printStackTrace();
-}
+        WebElement viewProductButton = driver.findElement(By.xpath("//a[@class='btn btn-primary']"));
+        viewProductButton.click();
 
-WebElement viewProductButton = driver.findElement(By.xpath(“[a@class=’btn btn-primary’]”));
-viewProductButton.click();
-WebElement quantityInput  = driver.findElement(By.id(“quantity_input”));
-quantityInput.clear();
-quantityInput.sendKeys(“4”);
-WebElement addToCartButton = driver.findElement(By.id(“add to cart button”));
-addToCartButton.click();
+        WebElement quantityInput = driver.findElement(By.id("quantity_input"));
+        quantityInput.clear();
+        quantityInput.sendKeys("4");
+
+        WebElement addToCartButton = driver.findElement(By.id("add_to_cart_button"));
+        addToCartButton.click();
+
         WebElement viewCartButton = driver.findElement(By.xpath("//a[@href='/cart']"));
         viewCartButton.click();
-WebElement cartProduct = driver.findElement(By.xpath("//td[contains(text(), 'Soft Streach Jeans')]"));
 
-        WebElement cartQuantity = driver.findElement(By.xpath("//td[contains(text(), 'SoftJeans')]/following-sibling::td[@class='quantity']"));
+        WebElement cartProduct = driver.findElement(By.xpath("//td[contains(text(), 'Soft Stretch Jeans')]"));
+        WebElement cartQuantity = driver.findElement(By.xpath("//td[contains(text(), 'Soft Stretch Jeans')]/following-sibling::td[@class='quantity']"));
 
         if (cartProduct.isDisplayed() && cartQuantity.getText().equals("4")) {
-            System.out.println("Product is displayed in cart with exact quantity.");
+            System.out.println("Product is displayed in the cart with the expected quantity.");
         } else {
-            System.out.println("Product is not displayed in cart with the expected quantity.");
+            System.out.println("Product is not displayed in the cart with the expected quantity.");
         }
+
         driver.quit();
     }
-
+}
