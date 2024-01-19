281a758b9902f86d3a00d37706863e72cdc01de9
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 13:49:26 +0530

    Update ProductReviewAutomation.java

--- Automation exclr/src/ProductReviewAutomation.java
+++ Automation exclr/src/ProductReviewAutomation.java
@@ -1,45 +1,53 @@
+package automationexercise;
 
-package SeleniumProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
 
-public class ProductReviewAutomation extends AbstractMethods {
-    WebDriver driver;{
+public class ProductReviewAutomation extends Abstract{
+    WebDriver driver;
+
     public static void main(String[] args) {
-        System.setProperty(""webdriver.chrome.driver", "E:/loopsautomation/src/main/resources/chromedriver.exe"");
+        System.setProperty("webdriver.chrome.driver", "D:\MOVIES\chromedriver-win64.zip\New folder\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://automationexercise.com");
-          driver.manage()timeouts().implicitlyWait (Duration.  oFSeconds(10));
-        WebElement productsButton = driver.findElement(By.xpath ("/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[2]/a[1]])");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement productsButton = driver.findElement(By.xpath("//li[@class='dropdown']/a[contains(text(),'Products')]"));
         productsButton.click();
-        WebElement viewProductButton = driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[2]/a[1]]"));
+
+        WebElement viewProductButton = driver.findElement(By.xpath("//a[contains(text(),'View Product')]"));
         viewProductButton.click();
-        WebElement writeReviewElement = driver.findElement(By.xpath(“/textarea[@id=review])"");
+
+        WebElement writeReviewElement = driver.findElement(By.xpath("//textarea[@id='input-review']"));
+
         if (writeReviewElement.isDisplayed()) {
-            
-            WebElement nameField = driver.findElement(By.xpath (“input[@id=name]""));
+            WebElement nameField = driver.findElement(By.xpath("//input[@id='input-name']"));
             nameField.sendKeys("Vinit");
 
-            WebElement emailField = driver.findElement(By.xpath (“input[@id=email]""));
+            WebElement emailField = driver.findElement(By.xpath("//input[@id='input-email']"));
             emailField.sendKeys("VinitM@gmail.com");
 
             writeReviewElement.sendKeys("This product is great!");
-            WebElement submitButton = driver.findElement(By.xpath ("button[@id='button-review']]"));
+
+            WebElement submitButton = driver.findElement(By.xpath("//button[@id='button-review']"));
             submitButton.click();
 
-            
-            WebElement successMessage = driver.findElement(By.xpath(“ span[normalize-space()=Thank you for your review.]""));
+            WebElement successMessage = driver.findElement(By.xpath("//span[normalize-space()='Thank you for your review.']"));
+
             if (successMessage.isDisplayed()) {
                 System.out.println("Review submitted successfully!");
-            } else {s
+            } else {
                 System.out.println("Review submission failed!");
             }
         } else {
             System.out.println("'Write Your Review' element is not visible.");
         }
+
         driver.quit();
     }
 }
