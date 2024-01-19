08b76982ff4f2f460b3c8726986cc07d280dc065
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-01-19 12:20:23 +0530

    Update AutomationExerciseExistingEmailSignup.java

--- Automation exclr/src/AutomationExerciseExistingEmailSignup.java
+++ Automation exclr/src/AutomationExerciseExistingEmailSignup.java
@@ -1,54 +1,43 @@
-package SeleniumProject;
+package automationexercise;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class AutomationExerciseExistingEmailSignup extends AbstractMethods {
-        WebDriver driver;{
 
+public class AutomatedTest extends Abstract {
+    public AutomatedTest(WebDriver driverhere) {
+		super(driverhere);
+	
+	}
 
-public static void main(String[]args)
-{
+	public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\MOVIES\\chromedriver-win64.zip\\New folder\\chromedriver-win64");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationexercise.com");
 
-System.setProperty”( "webdriver.chrome.driver", "E:/loopsautomation/src/main/resources/chromedriver.exe");
+        WebElement categoriesSidebar = driver.findElement(By.xpath("//h2[normalize-space()='Category']"));
+        if (categoriesSidebar.isDisplayed()) {
+            System.out.println("Categories are visible on the left sidebar");
+        }
 
-WebDriver driver = new ChromeDriver();
-Driver.get(http:automationexercise.com);
-WebElement homePageElement = driver.findElement(By.xpath(“[text()=’Home page Element]”));
+        WebElement womenCategory = driver.findElement(By.xpath("//a[normalize-space()='Women']"));
+        womenCategory.click();
 
-If (homePageElement.isDisplay()){
+        WebElement dressCategory = driver.findElement(By.xpath("//h2[@class='title text-center']"));
+        dressCategory.click();
 
-System.out.println(“Home page is visible successfully”);
-]
-Else{
-System.out.println(“Home page is not visible”);
+        WebElement categoryPageText = driver.findElement(By.xpath("//h2[@class='title text-center']"));
+        if (categoryPageText.getText().contains("Women-Top Product")) {
+            System.out.println("Category page for Women-Top Product is displayed.");
+        } else {
+            System.out.println("Category page for Women-Top Products is not displayed");
+        }
+
+        WebElement menSubCategory = driver.findElement(By.xpath("//a[normalize-space()='Men']"));
+        menSubCategory.click();
+
+        driver.quit();
+    }
 }
 
-WebElement signupLoginButton = driver.findElement(By.xpath(“button[contains(text(),Signup/Login)]”));
-signupLoginButton.click();
-WebElement newUserSignup = driver.findElement(By.xpath("[contains(text(),'New User Signup!')]"));
-If (newUserSignup.isDisplayed()){
-
-System.out.println(“New User is signup”);
-} else{
-System.out.println(“New User is signup!,is not visible”);
-}
-WebElement nameField= driver.fieldElement(by.id(RohitN@gmail.com)
-emailField.sendKeys(“RohitN@gmail.com);
-
-WebElement signupButton = driver.findElement(By.xpath(“button[conatains(text(),Signup)]”));
-signupButton.click();
-
-
-        WebElement errorElement = driver.findElement(By.xpath("[contains(text(),'Email Address already exist!')]"));
-
-If (errorElement.isDisplayed()){
-
-System.out.println(“Email is already exist”);
-}
-Else{
-System.out.println(“Email address alredy exist! Error is not visible”);
-}
-Driver.quite();
-}
-}
